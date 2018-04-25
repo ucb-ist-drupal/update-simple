88dde8414aba8ca32e018bf0f37de7c1e8d73648
Brian Wood <bwood@berkeley.edu>
2018-04-25 10:29:41 -0700

    fix quoting of --note option

--- update.sh
+++ update.sh
@@ -3,11 +3,16 @@ SITE=$1
 UPENV=$2 # is there a live env or just test?
 UPVER=$3
 RELEASE="Release $UPVER"
-# terminus -y env:clone-content $SITE.$UPENV dev && \
+
+echo "terminus -y upstream:updates:apply $SITE.dev --accept-upstream --updatedb && \
+terminus -y env:deploy $SITE.test --note=\"$RELEASE\" --updatedb && \
+"
+
 terminus -y upstream:updates:apply $SITE.dev --accept-upstream --updatedb && \
-terminus -y env:deploy $SITE.test --note $RELEASE --updatedb && \
+terminus -y env:deploy $SITE.test --note "$RELEASE" --updatedb && \
 if [ "$UPENV" == 'live' ]; then
-    terminus -y env:deploy $SITE.live --note $RELEASE --updatedb
+    echo "terminus -y env:deploy $SITE.live --note \"$RELEASE\" --updatedb"
+    terminus -y env:deploy $SITE.live --note "$RELEASE" --updatedb
 fi
 
 echo "Checking homepage HTTP status look for 200 at end:"
